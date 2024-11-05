8fadbac38debe5e68f7bab312b7f29acf90e77ac
SREERAJ313 <sreerajsj313@gmail.com>
2024-11-05 10:06:35 +0530

    slide show commit

---
+++ slide.js
@@ -0,0 +1,22 @@
+let slideIndex = 0;
+let slideintervel;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide-img");
+    
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) 
+      {
+      slides[i].style.display = "none";     
+      }
+       slides[slideIndex-1].style.display ="block" 
+      
+  }
+
+
